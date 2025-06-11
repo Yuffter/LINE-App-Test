a7bdb039c7b24b4497b18f6d3fbccbcf96a4be6e
Yuffter <yuupon2005@gmail.com>
2025-06-11 12:20:31 +0900

    LINEのIDを設定

--- src/LINESender.js
+++ src/LINESender.js
@@ -8,7 +8,8 @@ class LINESender {
     to - 送信先のユーザーID
     content - 送信するメッセージの内容
     */
-    sendMassage(to, content) {
+    sendMassage(content) {
+        const USER_LINE_ID = Config.USER_LINE_ID; /* 送り先のユーザID */
         // https://developers.line.biz/ja/reference/messaging-api/を参照してください
     }
 }
---
+++ src/useCase/airConditionerController.js
@@ -0,0 +1,9 @@
+function airConditionerOn() {
+  console.log("Air Conditioner is now ON");
+  // RemoClientを使用してエアコンをONにする処理を追加
+}
+
+function airConditionerOff() {
+  console.log("Air Conditioner is now OFF");
+    // RemoClientを使用してエアコンをOFFにする処理を追加
+}
