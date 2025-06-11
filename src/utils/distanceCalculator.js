94523cd74ba3f79a19508557253615f3a0d293c4
hiraifuto <hirai201ritu@gmail.com>
2025-06-11 16:20:58 +0900

    distance

--- src/utils/distanceCalculator.js
+++ src/utils/distanceCalculator.js
@@ -6,6 +6,14 @@ class DistanceCalculator {
     戻り値: 2座標間の距離（メートル単位）
     */
     static calculateDistance(lat1, lon1, lat2, lon2) {
-        
+        const toRadians = degrees => degrees * (Math.PI / 180);
+        const baseDistance = 111.111111;
+
+        const side1 = Math.abs(lat1 - lat2) * baseDistance;
+        const avgLon = (lon1 + lon2) / 2;
+        const side2 = baseDistance * Math.cos(toRadians(avgLon));
+
+        const distance = Math.sqrt(Math.pow(side1, 2) + Math.pow(side2, 2));
+        return distance;
     }
 }
