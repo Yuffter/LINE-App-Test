bbc073c54f3d7338658a1a8dcf9fad04695f3fd5
Yuffter <yuupon2005@gmail.com>
2025-06-11 11:58:12 +0900

    Configに気温と湿度の情報を追加

--- src/config.js
+++ src/config.js
@@ -47,4 +47,10 @@ class Config {
     static get REMO_LONGITUDE_CELL_ADDRESS() {
         return "B2";
     }
+    static get REMO_TEMPERATURE_CELL_ADDRESS() {
+        return "B3";
+    }
+    static get REMO_HUMIDITY_CELL_ADDRESS() {
+        return "B4";
+    }
 }
