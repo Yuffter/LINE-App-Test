e8ec43b4f0b460211fafb5ba56b263156a507570
hiraifuto <hirai201ritu@gmail.com>
2025-06-11 16:06:53 +0900

    onegai

--- src/utils/spreadSheetService.js
+++ src/utils/spreadSheetService.js
@@ -1,7 +1,12 @@
 class SpreadSheetService {
     // コンストラクタでスプレッドシートIDとシート名からシートを取得
     constructor(spreadsheetId, sheetName) {
-        
+        this.spreadsheet = SpreadsheetApp.openById(spreadsheetId);
+        this.sheet = this.spreadsheet.getSheetByName(sheetName);
+
+        if (!this.sheet) {
+        throw new Error(`シート名 "${sheetName}" が見つかりません。`);
+        }
     }
 
     // スプレッドシートからデータを取得するメソッド
@@ -10,7 +15,7 @@ class SpreadSheetService {
     戻り値: セルの値（例: '値1'）
     */
     readData(cellAddress) {
-
+        return this.sheet.getRange(cellAddress).getValue();
     }
 
     // スプレッドシートにデータを書き込むメソッド
@@ -19,6 +24,6 @@ class SpreadSheetService {
     values: 書き込む値の配列（例: ['値1', '値2']）
     */
     writeData(cellAddress, values) {
-
+        this.sheet.getRange(cellAddress).setValue(values);
     }
 }
