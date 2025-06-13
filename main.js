139a65e027348a98096ae0e5708e1c3556e93b0b
Yuffter <Yuffter@users.noreply.github.com>
2025-06-13 02:31:27 +0000

    deploy: cb4b7bc41eff92821ac9e40b24a6bd1bb91e51c1

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 document.getElementById("search-coordinate").addEventListener("click", function(event) {
     event.preventDefault(); // フォームのデフォルトの送信を防ぐ
+    showWaitingMessage(); // 位置情報取得中のメッセージを表示
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const lat = position.coords.latitude;
           const lon = position.coords.longitude;
-          alert(`緯度: ${lat}\n経度: ${lon}`);
+          // alert(`緯度: ${lat}\n経度: ${lon}`);
+          showCordinate(lat, lon); // 取得した座標を表示
         },
         (error) => {
           alert("位置情報の取得に失敗しました: " + error.message);
@@ -15,3 +17,13 @@ document.getElementById("search-coordinate").addEventListener("click", function(
       alert("このブラウザは位置情報に対応していません。");
     }
 });
+
+function showWaitingMessage() {
+  const message = document.getElementById("coordinate-output");
+  message.textContent = "位置情報を取得中...";
+}
+
+function showCordinate(lat, lon) {
+  const message = document.getElementById("coordinate-output");
+  message.textContent = `緯度: ${lat}, 経度: ${lon}`;
+}
