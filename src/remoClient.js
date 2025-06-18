17f030536ed41c741657b5d8ad8a15de7e297742
kuro8128 <tkenichi@gmail.com>
2025-06-18 14:52:11 +0900

    sendSignal更新

--- src/remoClient.js
+++ src/remoClient.js
@@ -32,31 +32,69 @@ class RemoClient {
     /*
     Nature Remo APIを使用してエアコンの操作を行うメソッド
     */
-    sendSignal() {
+    sendSignal(signal) {  //"ON", "OFF", or number
         const headers = {
             "Authorization": "Bearer " + this.accessToken,
             "Content-Type": "application/json"
         };
 
-        const options = {
+        const getOptions = {
             method: "get",
             headers: headers
         };
 
         // 家電情報を取得
-        const response = UrlFetchApp.fetch("https://api.nature.global/1/appliances", options);
-        const appliances = JSON.parse(response.getContentText());
-
-        console.log(appliances);
+        const getResponse = UrlFetchApp.fetch("https://api.nature.global/1/appliances", getOptions);
+        const appliances = JSON.parse(getResponse.getContentText());
 
         // nickname が "エアコン" のものを探す
+        let applianceId = "";
         for (let appliance of appliances) {
             if (appliance.nickname === "エアコン") {
-                Logger.log("エアコンのID: " + appliance.id);
-                return appliance.id;
+                applianceId = appliance.id;
+                break;
             }
         }
+        if (applianceId == "")
+            throw new Error("nickname が『エアコン』の家電が見つかりませんでした。");
 
-        throw new Error("nickname が『エアコン』の家電が見つかりませんでした。");
+        const url = 'https://api.nature.global/1/appliances/' + applianceId + '/aircon_settings';
+
+        let command;
+        if (signal == "ON") {
+            command = "ON";
+        } else if (signal == "OFF") {
+            command = "OFF";
+        } else {
+            command = Number(signal);
+        }
+
+        let payload = {}
+        let nowTemp;
+        switch (command) {
+            case "ON":
+                payload = {button: 'power-on'};
+                break;
+            case "OFF":
+                payload = {button: 'power-off'};
+                break;
+            default:
+                payload = {
+                    temperature: String(command),
+                    mode: "auto",
+                    fan: "auto",
+                };
+                break;
+        }
+
+        const postOptions = {
+            method: "post",
+            headers: headers,
+            payload: payload,
+            muteHttpExceptions: true
+        };
+
+        const postResponse = UrlFetchApp.fetch(url, postOptions);
+        Logger.log(postResponse.getContentText());
     }
 }
