9e4b5f63d4eaaf158636b1cfa9ad33ff622ddd4e
hiraifuto <hirai201ritu@gmail.com>
2025-06-18 13:34:54 +0900

    ラインからスプレッドシート入力

--- src/RequestReceiver.js
+++ src/RequestReceiver.js
@@ -16,19 +16,47 @@ function doPost(e) {
     const action = data.action;
 
     /* actionの値によって呼び出す関数を切り替える */
-    
+    switch (action) {
+      case 'UpdateUserLocation':
+        updateUserLocation(data);
+      case 'SetDistance':
+        setDistance(data);
+      case 'SetTemperature':
+        setTemperature(data);
+        
+      default:
+        return ContentService.createTextOutput('Unknown action').setMimeType(ContentService.MimeType.TEXT);
+    }
 }
 
 /*
 ユーザの現在の座標をスプレッドシートに更新する関数
 */
 function updateUserLocation(data) {
-    
+    const spreadsheetService = new SpreadSheetService(Config.SHEET_ID, Config.REMO_SHEET_NAME);
+
+    const latitude = data.latitude;
+    const longitude = data.longitude;
+
+    spreadsheetService.writeData(Config.REMO_LATITUDE_CELL_ADDRESS(),"latitude");
+    spreadsheetService.writeData(Config.REMO_LONGITUDE_CELL_ADDRESS(),"longitude");
 }
 
 /*
 設定をスプレッドシートに保存する関数
 */
-function setSettings(data) {
-    
+function setDistance(data) {
+    const spreadsheetService = new SpreadSheetService(Config.SHEET_ID, Config.REMO_SHEET_NAME);
+
+    const distance= data.distance;
+
+    spreadsheetService.writeData(Config.REMO_TEMPERATURE_CELL_ADDRESS(),"distance");
+}
+
+function setTemperature(data) {
+    const spreadsheetService = new SpreadSheetService(Config.SHEET_ID, Config.REMO_SHEET_NAME);
+
+    const Temperature= data.temperature;
+
+    spreadsheetService.writeData(Config.REMO_HUMIDITY_CELL_ADDRESS(),"temperature");
 }
